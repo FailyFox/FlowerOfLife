117dff30b57920fb1cec9cd200b9e8b36ffb198e
FailyFox <matviy2002ut@gmail.com>
2025-08-05 19:39:40 +0300

    added url appropriator for rest petal

---
+++ scripts/url_script.js
@@ -0,0 +1,16 @@
+switch (document.cookie) {
+    case "url=rest_petal.html":
+        document.getElementById("blue_envelope").setAttribute("href",
+            "indev.html");
+        document.getElementById("green_envelope").setAttribute("href",
+            "indev.html");
+        document.getElementById("red_envelope").setAttribute("href",
+            "indev.html");
+        document.getElementById("orange_envelope").setAttribute("href",
+            "indev.html");
+        document.getElementById("gold_envelope").setAttribute("href",
+            "indev.html");
+        document.getElementById("pink_envelope").setAttribute("href",
+            "indev.html");
+        break;
+}
