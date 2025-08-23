77a06522ace9cb086b64b6bfba41c66717498f85
FailyFox <matviy2002ut@gmail.com>
2025-08-23 15:59:36 +0300

    added room page

---
+++ scripts/room_script.js
@@ -0,0 +1,41 @@
+var img = new Image();
+img.src = 'pictures/room.png';
+document.cookie = "img=room";
+
+img.onload = function() {
+    var envelopes = document.querySelectorAll('.crystal-img');
+    var container = document.getElementById('container');
+    container.style.backgroundImage = 'url(' + img.src + ')';
+    container.style.width = img.width + 'px';
+    container.style.height = img.height + 'px';
+
+    function adjustContainerSize() {
+        var windowWidth = window.innerWidth;
+        var windowHeight = window.innerHeight;
+
+        var aspectRatio = img.width / img.height;
+
+        if (windowWidth / windowHeight > aspectRatio) {
+            container.style.width = windowHeight * aspectRatio + 'px';
+            container.style.height = windowHeight + 'px';
+        } else {
+            container.style.width = windowWidth + 'px';
+            container.style.height = windowWidth / aspectRatio + 'px';
+        }
+        
+        resizeEnvelopes();
+    }
+
+    function resizeEnvelopes() {
+        var envelopeSizeRatio = 0.04;
+        var envelopeWidth = container.offsetWidth * envelopeSizeRatio;
+
+        envelopes.forEach(function(envelope) {
+            envelope.style.width = envelopeWidth + 'px';
+        });
+    }
+
+    adjustContainerSize();
+
+    window.addEventListener('resize', adjustContainerSize);
+};
--- scripts/url_script.js
+++ scripts/url_script.js
@@ -1,5 +1,5 @@
 switch (document.cookie) {
-    case "url=rest_petal.html":
+    case "url=rest_petal.html; img=garden":
         document.getElementById("blue_envelope").setAttribute("href",
             "indev.html");
         document.getElementById("green_envelope").setAttribute("href",
@@ -11,6 +11,20 @@ switch (document.cookie) {
         document.getElementById("gold_envelope").setAttribute("href",
             "indev.html");
         document.getElementById("pink_envelope").setAttribute("href",
+            "indev.html");
+        break;
+    case "url=rest_petal.html; img=room":
+        document.getElementById("crystal1").setAttribute("href",
+            "indev.html");
+        document.getElementById("crystal2").setAttribute("href",
+            "indev.html");
+        document.getElementById("crystal3").setAttribute("href",
+            "indev.html");
+        document.getElementById("crystal4").setAttribute("href",
+            "indev.html");
+        document.getElementById("crystal5").setAttribute("href",
+            "indev.html");
+        document.getElementById("crystal6").setAttribute("href",
             "indev.html");
         break;
 }
