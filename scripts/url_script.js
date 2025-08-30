52438a34f081359130567c50ad8e001c647f47e9
FailyFox <matviy2002ut@gmail.com>
2025-08-30 13:39:50 +0300

    updated project structure

--- scripts/url_script.js
+++ scripts/url_script.js
@@ -1,30 +1,30 @@
 switch (document.cookie) {
-    case "url=rest_petal.html; img=garden":
+    case "img=garden; url=rest_petal.html":
         document.getElementById("blue_envelope").setAttribute("href",
-            "indev.html");
+            "../indev.html");
         document.getElementById("green_envelope").setAttribute("href",
-            "indev.html");
+            "../indev.html");
         document.getElementById("red_envelope").setAttribute("href",
-            "indev.html");
+            "../indev.html");
         document.getElementById("orange_envelope").setAttribute("href",
-            "indev.html");
+            "../indev.html");
         document.getElementById("gold_envelope").setAttribute("href",
-            "indev.html");
+            "../indev.html");
         document.getElementById("pink_envelope").setAttribute("href",
-            "indev.html");
+            "../indev.html");
         break;
-    case "url=rest_petal.html; img=room":
+    case "img=room; url=rest_petal.html":
         document.getElementById("crystal1").setAttribute("href",
-            "indev.html");
+            "../indev.html");
         document.getElementById("crystal2").setAttribute("href",
-            "indev.html");
+            "../indev.html");
         document.getElementById("crystal3").setAttribute("href",
-            "indev.html");
+            "../indev.html");
         document.getElementById("crystal4").setAttribute("href",
-            "indev.html");
+            "../indev.html");
         document.getElementById("crystal5").setAttribute("href",
-            "indev.html");
+            "../indev.html");
         document.getElementById("crystal6").setAttribute("href",
-            "indev.html");
+            "../indev.html");
         break;
 }
